e494cc62f1cda597c57b6402d639af9a1686d59b
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2017-02-06 21:13:03 +0200

    add two more excercizes

---
+++ ex4.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+fs.readFile(process.argv[2], function(err, data){
+    // console.log(data);
+    console.log(data.toString().split('\n').length - 1);
+});
+
+
---
+++ ex5.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(process.argv[2], function (err, list) {
+    for(let i=0; i < list.length; i++) {
+        if (path.extname(list[i]) == '.' + process.argv[3]) {
+            console.log(list[i]);
+        }
+    }
+});
